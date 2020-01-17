f35efdc04c73b0a877bb34612c5aceaf97c0bbcb
Austin Osorio <osorio_austin@yahoo.com>
2020-01-17 06:37:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
 
   reversed_str = ""
   split_array = str.split(" ")
-  split_array.each {|x| x.reverse += reversed_str}
+  split_array.each {|x| x.reverse! += reversed_str}
 
   reversed_str
 end
